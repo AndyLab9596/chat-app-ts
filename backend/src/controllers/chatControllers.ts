e7eaa92d67214f2f9abe41471195cb02e2e2d8f6
andyvy <thienvy95@gmail.com>
2022-08-27 15:04:17 +0700

    add remove in group chat

--- backend/src/controllers/chatControllers.ts
+++ backend/src/controllers/chatControllers.ts
@@ -42,8 +42,138 @@ const accessChat = async (req: Request, res: Response) => {
             throw new BadRequestError('Something went wrong')
         }
     }
+};
+
+const fetchChats = async (req: Request, res: Response) => {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+        .populate('users')
+        .populate('groupAdmin')
+        .populate('latestMessage')
+        .sort({ updatedAt: -1 })
+        .then(async (results) => {
+            const chat = await User.populate(results, {
+                path: "latestMessage.sender",
+                select: 'name pic email'
+            })
+
+            res.status(StatusCodes.OK).send(chat)
+        });
+};
+
+const createGroupChat = async (req: Request, res: Response) => {
+    if (!req.body.users || !req.body.name) {
+        throw new BadRequestError('Please fill all the fields')
+    }
+
+    let users = JSON.parse(req.body.users);
+
+    if (users.length < 2) {
+        throw new BadRequestError('More than 2 users are required to form a group chat');
+    }
+
+    users.push(req.user);
+
+    const groupChat = await Chat.create({
+        chatName: req.body.name,
+        users: users,
+        isGroupChat: true,
+        groupAdmin: req.user,
+    })
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+        .populate("users")
+        .populate("groupAdmin");
+
+    res.status(StatusCodes.CREATED).json(fullGroupChat);
+};
+
+const renameGroup = async (req: Request, res: Response) => {
+    const { chatId, chatName } = req.body;
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName,
+        },
+        {
+            new: true,
+        }
+    )
+        .populate("users")
+        .populate("groupAdmin");
+    if (!updatedChat) throw new BadRequestError('Something went wrong');
+    res.status(StatusCodes.OK).json(updatedChat);
+}
+
+const addToGroup = async (req: Request, res: Response) => {
+    const { chatId, userId } = req.body;
+
+    // const isUserAlreadyInGroupChat = await Chat.find({
+    //     chatId,
+    //     isGroupChat: true,
+    //     $and: [
+    //         { users: { $elemMatch: { $eq: req.user._id } } },
+    //         { users: { $elemMatch: { $eq: userId } } },
+    //     ],
+    // })
+
+    // if (isUserAlreadyInGroupChat.length === 0) {
+    //     const groupChat = await Chat.findByIdAndUpdate(
+    //         chatId,
+    //     )
+    //         .populate("users")
+    //         .populate("groupAdmin");
+    //     if (!groupChat) throw new BadRequestError('Something went wrong');
+    //     res.status(StatusCodes.OK).json(groupChat);
+    // } else {
+    //     const added = await Chat.findByIdAndUpdate(
+    //         chatId,
+    //         {
+    //             $push: { users: userId },
+
+    //         },
+    //         { new: true }
+    //     )
+    //         .populate("users")
+    //         .populate("groupAdmin");
+    //     if (!added) throw new BadRequestError('Something went wrong');
+    //     res.status(StatusCodes.CREATED).json(added);
+    // }
+
+    const added = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $push: { users: userId },
+
+        },
+        { new: true }
+    )
+        .populate("users")
+        .populate("groupAdmin");
+    if (!added) throw new BadRequestError('Something went wrong');
+    res.status(StatusCodes.CREATED).json(added);
+}
+
+const removeFromGroup = async (req: Request, res: Response) => {
+    const { chatId, userId } = req.body;
+
+    const removed = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $pull: { users: userId },
+
+        },
+        { new: true }
+    )
+        .populate("users")
+        .populate("groupAdmin");
+    if (!removed) throw new BadRequestError('Something went wrong');
+    res.status(StatusCodes.OK).json(removed);
 }
 
 export {
     accessChat,
+    fetchChats,
+    createGroupChat,
+    renameGroup,
+    addToGroup,
+    removeFromGroup
 }
--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import { accessChat } from "../controllers/chatControllers";
+import { accessChat, createGroupChat, fetchChats, renameGroup, addToGroup, removeFromGroup } from "../controllers/chatControllers";
 import auth from "../middleware/authenticateUser";
 
 
 const router = Router();
 
 router.route('/').post(auth, accessChat);
-// router.route('/').get(auth, fetchChats);
-// router.route('/group').post(auth, createGroupChat);
-// router.route('/rename').patch(auth, renameGroup);
-// router.route('/groupremove').patch(auth, removeFromGroup);
-// router.route('/groupadd').patch(auth, addToGroup);
+router.route('/').get(auth, fetchChats);
+router.route('/group').post(auth, createGroupChat);
+router.route('/rename').put(auth, renameGroup);
+router.route('/groupremove').put(auth, removeFromGroup);
+router.route('/groupadd').put(auth, addToGroup);
 
 export default router;
